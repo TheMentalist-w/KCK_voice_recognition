5befefbd04a3fdc987a7377c1595837b8fa0cbc1
Julia <jula.stachowiak@gmail.com>
2021-01-04 23:43:38 +0100

    remove unused imports, adjust syntax to be more pythonic

--- inf141286_inf141313.py
+++ inf141286_inf141313.py
@@ -1,31 +1,31 @@
 import scipy
 from scipy import signal
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.io import wavfile
-import wave, struct
-import cmath
+import wave
+import struct
 import sys
-import os.path
 from os import path
 
+
 def main():
-    if ((len(sys.argv) < 2) or (len(sys.argv) > 2)):
+    if len(sys.argv) != 2:
         print("Wrong number of arguments")
         return
     filename = sys.argv[1]
-    if (path.exists(filename) == False):
+
+    try:
+        y1, signal1 = scipy.io.wavfile.read(filename)
+    except OSError:
         print("Wrong file path")
         return
-    
-    y1, signal1 = scipy.io.wavfile.read(filename)
+
     window = signal.tukey(len(signal1))
     signalWindow = window * signal1
     signalFFT = np.fft.fft(signalWindow)
     signalFFTabs = np.log(np.abs(signalFFT))
-    maxValue1 = max(signalFFTabs)
-    maxValue2 = maxValue1
+    maxValue2 = maxValue1 = max(signalFFTabs)
     product = [0 for i in range(int(len(signal1)/8))]
     for i in range(int(len(signal1)/8)):
         product[i] = signalFFTabs[i] * signalFFTabs[2*i] * signalFFTabs[3*i] * signalFFTabs[4*i]
@@ -34,12 +34,9 @@ def main():
             fundFreq = i
     fundFreq1 = fundFreq * y1 / len(signal1)
     
-    if (fundFreq1 >= 165):
-        answer = 'K'
-    else:
-        answer = 'M'
-    
+    answer = 'K' if fundFreq1 >= 165 else 'M'
     print(answer)
+
 
 if __name__ == "__main__":
     main()
