16ec3f055de1291ac9898615868b039f9b771d56
TheMentalist-w <50791355+TheMentalist-w@users.noreply.github.com>
2021-01-11 10:30:03 +0100

    remove imports

--- inf141286_inf141313.py
+++ inf141286_inf141313.py
@@ -1,39 +1,40 @@
-import scipy
 from scipy import signal
-import numpy as np
-import pandas as pd
+from numpy import fft, log, zeros
+#import numpy as np
+#import pandas as pd
 from scipy.io import wavfile
-import wave
-import struct
-import sys
+#import wave
+#import struct
+from sys import argv
 from os import path
 
+def process(signal1):
+    window = signal.tukey(len(signal1))
+    signalWindow = window * signal1
+    signalFFT = fft.fft(signalWindow)
+    signalFFTabs = log(abs(signalFFT))
+    return signalFFTabs
 
 def main():
-    if len(sys.argv) != 2:
+    if len(argv) != 2:
         print("Wrong number of arguments")
         return
-    filename = sys.argv[1]
+    filename = argv[1]
 
     try:
-        y1, signal1 = scipy.io.wavfile.read(filename)
+        y1, signal1 = wavfile.read(filename)
     except OSError:
         print("Wrong file path")
         return
-
-    window = signal.tukey(len(signal1))
-    signalWindow = window * signal1
-    signalFFT = np.fft.fft(signalWindow)
-    signalFFTabs = np.log(np.abs(signalFFT))
-    maxValue2 = maxValue1 = max(signalFFTabs)
-    product = [0 for i in range(int(len(signal1)/8))]
+    signalFFTabs = process(signal1)
+    maxValue2 = 0
+    product = zeros(int(len(signal1)/8), dtype=float)
     for i in range(int(len(signal1)/8)):
         product[i] = signalFFTabs[i] * signalFFTabs[2*i] * signalFFTabs[3*i] * signalFFTabs[4*i]
         if ((product[i] > maxValue2) and (i >= (65 * len(signal1)/y1)) and (i <= (275 * len(signal1)/y1))):
             maxValue2 = product[i]
             fundFreq = i
     fundFreq1 = fundFreq * y1 / len(signal1)
-    
     answer = 'K' if fundFreq1 >= 165 else 'M'
     print(answer)
 
